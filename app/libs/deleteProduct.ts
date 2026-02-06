0c90bd3aa8c80af2e7c7e189e4ce1579fb1d0f88
Moroundiya <moroundiya@outlook.com>
2026-02-06 13:29:28 +0100

    Delete Product

--- app/libs/deleteProduct.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function deleteProduct(id: string) {
-	const res = await axios
-		.delete(`/api/deleteproduct/${id}`)
-		.then((res) => res.data)
-		.catch((err) => err.message);
-	return res;
-}
